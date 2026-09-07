2138e5fbb5fd4690243c0f5aea9fea13068edabb
Nina Dragićević <cxxx1828@gmail.com>
2026-09-07 11:25:24 +0200

    Update app.cpp

--- iot_mqtt_finale/iot/app.cpp
+++ iot_mqtt_finale/iot/app.cpp
@@ -1,18 +1,14 @@
 #include <iostream>
-#include "httplib.h" // HTTP library za c++
+#include "httplib.h" 
 
 int main() {
-    // Kreirati HTTP klijenta
     httplib::Client client("http://localhost:8080");
     
-    //Slanje HTTP GET zahteva na server da bi se dobilo trenutno stanje okruzenja
     auto res = client.Get("/environment");
     
-    // Provera da li je zahtev uspesno obradjen
     if (res && res->status == 200) {
         
         std::cout << std::endl;
-        // Ispis tela odgovora (stanje okruzenja)
         std::cout << "Current environment status:\n" << res->body << std::endl;
         std::cout << std::endl;
     } else {
